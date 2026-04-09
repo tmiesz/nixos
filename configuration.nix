22dc05fe6772ef2bcd82d79c9ab1777e10fc08e3
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-04-10 00:24:40 +0200

    move pipewire

--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ./core/pipewire.nix
   ];
 
   boot.loader.systemd-boot.enable = true;
@@ -37,15 +38,6 @@
   services.xserver.xkb = {
     layout = "us";
     variant = "";
-  };
-
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
   };
 
   users.users.wiqht = {
