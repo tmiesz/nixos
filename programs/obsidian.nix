e9435cee24af0cb8e2efb8a7f34e680b3957dfd9
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-04-19 17:14:35 +0200

    add vaults to obisidan

--- programs/obsidian.nix
+++ programs/obsidian.nix
@@ -1,5 +1,20 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-  home.packages = [ pkgs.obsidian ];
+  programs.obsidian = {
+    enable = true;
+
+    defaultSettings.app = {
+      newFileLocation = "folder";
+      newFileFolderPath = "unsorted";
+      attachmentFolderPath = "files";
+    };
+
+    vaults = {
+      "learning".target = "Notes/learning";
+      "work".target = "Notes/work";
+      "projects".target = "Notes/projects";
+      "general".target = "Notes/general";
+    };
+  };
 }
