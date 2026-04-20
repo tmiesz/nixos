2bc792317bd6bcc87508f190aabef0ca4d00ea51
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-04-20 17:25:20 +0200

    duplication

--- configuration.nix
+++ configuration.nix
@@ -30,10 +30,6 @@
     LC_TIME = "pl_PL.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager = {
     sddm.enable = true;
