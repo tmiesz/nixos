fcf34b49c3dd048ad8d84a29c5f65d439ad2fb86
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-04-20 17:25:27 +0200

    gc update

--- core/garbage-collection.nix
+++ core/garbage-collection.nix
@@ -4,7 +4,7 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 7d";
+    options = "--delete-older-than 14d";
   };
 
   nix.settings.auto-optimise-store = true;
