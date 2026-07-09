d867cfa386029021489a9f6919756dc653e3e1b9
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-07-09 20:56:27 +0200

    flake modification, no changes

--- flake.nix
+++ flake.nix
@@ -4,6 +4,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
 
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-26.05";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,6 +18,7 @@
     };
 
     # TODO: encryption
+    # or agenix?
     # sops-nix = {
     #   url = "github:mic92/sops-nix";
     #   inputs.nixpkgs.follows = "nixpkgs";
