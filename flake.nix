ddd7abaceb0d312c72dfa204c6290ba65c8d7d41
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-07-11 17:12:33 +0200

    small refactor

--- flake.nix
+++ flake.nix
@@ -44,14 +44,16 @@
 
           home-manager.nixosModules.home-manager
           {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-            home-manager.users.wiqht = {
-              imports = [
-                doom-emacs.homeModule
-                ./home.nix
-              ];
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = { inherit inputs; };
+              users.wiqht = {
+                imports = [
+                  doom-emacs.homeModule
+                  ./home.nix
+                ];
+              };
             };
           }
         ];
