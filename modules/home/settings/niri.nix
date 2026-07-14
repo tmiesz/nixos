40de91036f2d30e2c47247168d2cd2ebd89d51a5
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-07-14 21:17:12 +0200

    add niri features

--- modules/home/settings/niri.nix
+++ modules/home/settings/niri.nix
@@ -5,15 +5,28 @@
     output "DP-6" {
       mode "1920x1080@143.614"
       transform "90"
-      position x=0 y=0
+      position x=0 y=-768
       scale 1
     }
 
     output "DP-4" {
       mode "2560x1440@360.001"
       position x=1080 y=0
-      scale 1
+      scale 1.25
     }
+
+    layout {
+      gaps 6
+
+      focus-ring {
+        width 3
+        active-color "#A8AEFF"
+        inactive-color "#505050"
+      }
+    }
+
+    spawn-at-startup "spotify"
+    spawn-at-startup "discord"
 
     binds {
       // Apps
@@ -27,10 +40,14 @@
       Mod+M { maximize-window-to-edges; }
 
       // Focus
-      Mod+H { focus-column-left; }
-      Mod+L { focus-column-right; }
+      // Mod+H { focus-column-left; }
+      // Mod+L { focus-column-right; }
+      Mod+H { focus-column-or-monitor-left; }
+      Mod+L { focus-column-or-monitor-right; }
       Mod+K { focus-window-up; }
       Mod+J { focus-window-down; }
+
+      Pause { spawn "wpctl" "set-mute" "@DEFAULT_AUDIO_SOURCE@" "toggle"; }
     }
   '';
 }
