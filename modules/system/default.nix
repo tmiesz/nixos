1ba93b9a53656cb126d5d27e0fdab4a7d5414b88
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-07-19 12:37:30 +0200

    usre nautilus, mvoe openrgb

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,9 +1,14 @@
 { ... }:
 
 {
+  services.hardware.openrgb.enable = true;
+
   programs = {
     steam.enable = true;
     gamemode.enable = true;
     zsh.enable = true;
+
+    niri.enable = true;
+    niri.useNautilus = true;
   };
 }
