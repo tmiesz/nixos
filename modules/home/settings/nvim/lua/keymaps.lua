6ab74cd96628e016cb3cda0a19c39bc480797876
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-08-12 17:18:41 +0200

    chore: change keymaps

--- modules/home/settings/nvim/lua/keymaps.lua
+++ modules/home/settings/nvim/lua/keymaps.lua
@@ -7,12 +7,12 @@ vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover docs" })
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Show references" })
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
-vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
+vim.keymap.set("n", "<leader>sd", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
 
 vim.keymap.set("n", "<leader>gg", ":Neogit<cr>", { desc = "Open Neogit" })
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
-vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format({ async = true }) end, { desc = "Format buffer" })
+vim.keymap.set("n", "<leader>cf", function() vim.lsp.buf.format({ async = true }) end, { desc = "Format code" })
 
 vim.keymap.set("x", "p", [["_dP]], { desc = "Paste without yanking" })
 vim.keymap.set({ "n", "v" }, "<leader>x", "_d", { desc = "Delete without yanking" })
@@ -22,8 +22,8 @@ vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 
-vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split window vertically" })
-vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>wv", ":vsplit<CR>", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>wh", ":split<CR>", { desc = "Split window horizontally" })
 
 vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
 vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
@@ -44,7 +44,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Move down in buffer with curso
 vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result with cursor centered" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result with cursor centered" })
 
-vim.keymap.set("n", "<leader>ra", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace word cursor is on globally" })
+vim.keymap.set("n", "<leader>ra", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace every word occurence in the buffer." })
 
 -- undo tree
 vim.keymap.set("n", "<leader>u", function()
