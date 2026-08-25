c50c69be44e7043cd886fd2463f69758bc3a8ed3
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-08-25 18:13:55 +0200

    feat(tmux): add tmux

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -18,6 +18,7 @@
     neovim.enable = true;
     doom-emacs.enable = true;
     discord.enable = true;
+    tmux.enable = true;
   };
 
   home.packages = with pkgs; [
---
+++ modules/home/settings/tmux.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }:
+
+{
+  programs.tmux = {
+    extraConfig = ''
+      set -g default-shell ${pkgs.zsh}/bin/zsh
+    '';
+  };
+}
