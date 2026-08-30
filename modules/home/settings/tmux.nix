cb8739716426d69219864728aeaa2e0caff3d1cb
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-08-30 11:08:09 +0200

    feat(tmux): vim navigations and base settings

--- modules/home/settings/tmux.nix
+++ modules/home/settings/tmux.nix
@@ -2,6 +2,17 @@
 
 {
   programs.tmux = {
+
+    shortcut = "Space";
+    baseIndex = 1;
+    clock24 = true;
+    keyMode = "vi";
+    terminal = "xterm-256color";
+
+    plugins = with pkgs.tmuxPlugins; [
+      vim-tmux-navigator
+    ];
+
     extraConfig = ''
       set -g default-shell ${pkgs.zsh}/bin/zsh
     '';
