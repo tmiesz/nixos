b29559830c375f28766b52cd565bf73c66a96ab5
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-09-02 23:21:46 +0200

    feat(tmux): catppuccin styling and sensible

--- modules/home/settings/tmux.nix
+++ modules/home/settings/tmux.nix
@@ -8,13 +8,22 @@
     clock24 = true;
     keyMode = "vi";
     terminal = "xterm-256color";
+    mouse = true;
+    shell = "${pkgs.zsh}/bin/zsh";
 
     plugins = with pkgs.tmuxPlugins; [
       vim-tmux-navigator
+      catppuccin
+      sensible
     ];
 
     extraConfig = ''
-      set -g default-shell ${pkgs.zsh}/bin/zsh
+      set-option -g status-position top
+
+      set -g status-right-length 100
+      set -g status-left-length 100
+      set -g status-left ""
+      set -g status-right "#{E:@catppuccin_status_session}"
     '';
   };
 }
