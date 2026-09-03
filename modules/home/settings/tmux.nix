492de7b62874139c4be88a7df2cffde8220d7f75
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-09-03 15:20:57 +0200

    feat(tmux): add resurrect to keep sessions between boots

--- modules/home/settings/tmux.nix
+++ modules/home/settings/tmux.nix
@@ -13,17 +13,31 @@
 
     plugins = with pkgs.tmuxPlugins; [
       vim-tmux-navigator
-      catppuccin
       sensible
+
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_window_text "#W"
+          set -g @catppuccin_window_current_text "#W"
+
+          set -g status-right-length 100
+          set -g status-left-length 100
+          set -g status-left ""
+          set -g status-right "#{E:@catppuccin_status_session}"
+        '';
+      }
+
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-nvim 'session'
+        '';
+      }
     ];
 
     extraConfig = ''
       set-option -g status-position top
-
-      set -g status-right-length 100
-      set -g status-left-length 100
-      set -g status-left ""
-      set -g status-right "#{E:@catppuccin_status_session}"
     '';
   };
 }
