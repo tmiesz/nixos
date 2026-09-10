e960b8b035befd1bd98685b6177b5b7a1348940d
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-09-10 23:35:14 +0200

    feat(neovim): add placeholder icons, *stolen from normalnvim*

---
+++ modules/home/settings/nvim/lua/utils/icons.lua
@@ -0,0 +1,109 @@
+return {
+  -- Which-key
+  Debugger = "",
+  Run = "󰑮",
+  Find = "",
+  Session = "󱂬",
+  Sort = "󰒺",
+  Buffer = "󰓩",
+  Terminal = "",
+  UI = "",
+  Test = "󰙨",
+  Packages = "󰏖",
+  Docs = "",
+  Git = "󰊢",
+  LSP = "",
+
+  -- Heirline-components - tabline
+  BufferClose = "󰅖",
+  FileModified = "",
+  FileReadOnly = "",
+  ArrowLeft = "",
+  ArrowRight = "",
+  TabClose = "󰅙",
+
+  -- Heirline-components - winbar
+  CompilerPlay = "",
+  CompilerStop = "",
+  CompilerRedo = "",
+  NeoTree = "",
+  Aerial = "" ,
+  ZenMode = "󰰶",
+  BufWrite = "",
+  BufWriteAll = "",
+  Ellipsis = "…",
+  BreadcrumbSeparator = "",
+
+  -- Heirline-components - statuscolumn
+  FoldClosed = "",
+  FoldOpened = "",
+  FoldSeparator = " ",
+
+  -- Heirline-components - statusline
+  ActiveLSP = "",
+  ActiveTS = "",
+  Environment = "",
+  DiagnosticError = "",
+  DiagnosticHint = "󰌵",
+  DiagnosticInfo = "󰋼",
+  DiagnosticWarn = "",
+  LSPLoading1 = "",
+  LSPLoading2 = "󰀚",
+  LSPLoading3 = "",
+  SearchCount = "",
+  MacroRecording = "",
+  ToggleResults = "󰑮",
+
+  -- Heirline-components - misc
+  Paste = "󰅌",
+  PathSeparator = "",
+
+  -- Neotree
+  FolderClosed = "",
+  FolderEmpty = "",
+  FolderOpen = "",
+  Diagnostic = "󰒡",
+  DefaultFile = "󰈙",
+
+  -- Git
+  GitBranch = "",
+  GitAdd = "",
+  GitChange = "",
+  GitDelete = "",
+  GitConflict = "",
+  GitIgnored = "◌",
+  GitRenamed = "➜",
+  GitSign = "▎",
+  GitStaged = "✓",
+  GitUnstaged = "✗",
+  GitUntracked = "★",
+
+  -- DAP
+  DapBreakpoint = "",
+  DapBreakpointCondition = "",
+  DapBreakpointRejected = "",
+  DapLogPoint = ".>",
+  DapStopped = "󰁕",
+
+  -- Telescope
+  PromptPrefix = "❯",
+
+  -- Nvim-lightbulb
+  Lightbulb = "💡",
+
+  -- Alpha
+  GreeterNew = "📄",
+  GreeterRecent = "🌺",
+  GreeterYazi = "🦆",
+  GreeterSessions = "🔎",
+  GreeterProjects = "💼",
+  GreeterPlug = "",
+
+  -- Mason
+  MasonInstalled = "✓",
+  MasonUninstalled = "✗",
+  MasonPending = "⟳",
+
+  -- Render-markdown
+  RenderMarkdown = { ' ', ' ', '󰲥 ', '󰲧 ', '󰲩 ', '󰲫 ' }
+}
